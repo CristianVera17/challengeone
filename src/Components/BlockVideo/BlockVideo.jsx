65adf82ca05eb51dc19e7a588aed7881dde20542
cristiandeveloper1117 <cristian.vera@arbusta.net>
2023-01-16 16:34:26 -0300

    015-000 View MVP in progress

--- src/Components/BlockVideo/BlockVideo.jsx
+++ src/Components/BlockVideo/BlockVideo.jsx
@@ -1,11 +1,127 @@
 import React from "react";
 import "./block-video.css";
 
-
 export default function BlockVideo() {
   return (
-    <div className="box-video">
-      <iframe title="marvelview" className="iframe-css" src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"width="640" height="360" allowfullscreen frameborder="0"></iframe>`
-    </div>
+    <>
+      <div className="box-view">
+        <div className="box-view-int">
+          <div className="view-one">
+            <iframe
+              title="marvelview"
+              className="iframe-css"
+              scrolling="no"
+              src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"
+              width="640"
+              height="360"
+              allowfullscreen
+              frameborder="0"
+            ></iframe>
+            `
+          </div>
+          <div className="view-two">
+            <div className="box-scroll">
+              <div className="box-int-scroll">
+                <div className="card-scroll">
+                  <iframe
+                    title="marvelview"
+                    className="iframe-view"
+                    scrolling="no"
+                    src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"
+                    width="640"
+                    height="360"
+                    allowfullscreen
+                    frameborder="0"
+                  ></iframe>
+                  `
+                </div>
+
+                <div className="card-scroll">
+                  <iframe
+                    title="marvelview"
+                    className="iframe-view"
+                    scrolling="no"
+                    src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"
+                    width="640"
+                    height="360"
+                    allowfullscreen
+                    frameborder="0"
+                  ></iframe>
+                  `
+                </div>
+
+                <div className="card-scroll">
+                  <iframe
+                    title="marvelview"
+                    className="iframe-view"
+                    scrolling="no"
+                    src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"
+                    width="640"
+                    height="360"
+                    allowfullscreen
+                    frameborder="0"
+                  ></iframe>
+                  `
+                </div>
+
+                <div className="card-scroll">
+                  <iframe
+                    title="marvelview"
+                    className="iframe-view"
+                    scrolling="no"
+                    src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"
+                    width="640"
+                    height="360"
+                    allowfullscreen
+                    frameborder="0"
+                  ></iframe>
+                  `
+                </div>
+
+                <div className="card-scroll">
+                  <iframe
+                    title="marvelview"
+                    className="iframe-view"
+                    scrolling="no"
+                    src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"
+                    width="640"
+                    height="360"
+                    allowfullscreen
+                    frameborder="0"
+                  ></iframe>
+                  `
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="box-text-view-one">
+        <div className="view-one-text">
+          <div className="one-text-view">
+            <div>
+              <p>MARVEL'S LONG STORY SHORT</p>
+            </div>
+            <div>
+              <p className="text-2xl font-bold">
+                The Secret Invasion Was Caused By Hypnotized Cows?! | Long Story
+                Short
+              </p>
+            </div>
+            <div>
+              <p>
+                Learn how Tony Stark and a ragtag group of heroes and villains
+                stopped Earth from being taken over by an alien race!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="box-video">
+        {/* <iframe title="marvelview" className="iframe-css" src="https://assets.espn.go.com/players/web-player-bundle/next/embed/share.html#id=6315795499112&brand=marvel"width="640" height="360" allowfullscreen frameborder="0"></iframe>` */}
+      </div>
+    </>
   );
 }
