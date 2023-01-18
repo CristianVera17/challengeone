e7c27869d2733674b7c8ff1e6a7501781ee3ec80
cristiandeveloper1117 <cristian.vera@arbusta.net>
2023-01-18 16:00:59 -0300

    019-000 Scroll up Navbar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,17 +5,48 @@ import BlockTree from "../Components/blockTree/BlockTree";
 import BlockTwo from "../Components/blockTwo/BlockTwo";
 import BlockVideo from "../Components/BlockVideo/BlockVideo";
 import NavbarOne from "../Components/Nav/NavbarOne";
+import "../Styles/home-page.css"
 
 export default function HomePage() {
+  let ubicacionPrincipal  = window.pageYOffset;
+window.onscroll = function() {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if(ubicacionPrincipal >= Desplazamiento_Actual){
+        document.getElementById('navbar').style.top = '0';
+    }
+    else{
+        document.getElementById('navbar').style.top = '-200px';
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+}
   return (
-    <div className="h-96">
-      <NavbarOne />
+    <div>
+      {/* <NavbarOne />
       <AutomaticSlider/>
       <BlockTwo />
       <BlockTree />
       <BlockVideo />
       <BlockTwo />
-      <BlockFooter/>
+      <BlockFooter/> */}
+       <header>
+        <nav id="navbar">
+            <div class="logo">
+                {/* <img src="logo.png" alt=""> */}
+            </div>
+            <div class="navegacion">
+                <NavbarOne/>
+            </div>
+        </nav>
+    </header>
+    <div className="text-section">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id est voluptate in corporis asperiores sequi adipisci neque eos,
+        provident temporibus reprehenderit aspernatur dignissimos perferendis maiores commodi voluptatum quos accusantium,
+        excepturi alias. Corrupti unde fuga eveniet, dicta a aliquam nobis ipsam aliquid quasi beatae optio architecto at
+        consequatur libero qui commodi eos vitae harum numquam facere distinctio culpa ex aut! Eaque dolor commodi ab magnam,
+        animi fugit ducimus iste fuga nihil temporibus possimus. Accusantium, ratione minima dolor porro officiis excepturi
+        libero dolore. Hic at deleniti quis nesciunt sit illo natus repellendus, dignissimos tempora numquam facere sequi
+        aliquam modi eaque animi corporis?
+    </div>
     </div>
   );
 }
