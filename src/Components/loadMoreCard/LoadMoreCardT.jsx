c7d714f664c93538230d65a302e29ed609e32778
cristiandeveloper1117 <cristian.vera@arbusta.net>
2023-01-20 13:16:04 -0300

    025-000 Loadmore MVP in progress

--- src/Components/loadMoreCard/LoadMoreCardT.jsx
+++ src/Components/loadMoreCard/LoadMoreCardT.jsx
@@ -11,6 +11,8 @@ export const LoadMoreCardT = () => {
     setVisible((prevValue) => prevValue + 3);
   };
 
+
+
   useEffect(() => {
     axios
       .get(
@@ -27,22 +29,41 @@ export const LoadMoreCardT = () => {
       <div className="box-card-load">
 
         <div className="box-card-im">
-          <h1>Hola</h1>
+
+          <div className="box-the-latest">
+            <div>
+            <div className="sets__eyebrow-two">
+             THE LATEST
+            </div>
+            </div>
+          </div>
+
+
           {items.slice(0, visible).map((item) => (
             <div className="box-load-card">
               <div className="card-load">
-                <div className="id">
-                  <img
-                    src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                    alt=""
-                  />
+                <div className="int-box-card-load">
+                 
+                  <div className="load-img-card">
+                      <img
+                      className="load-img-css"
+                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                      alt=""
+                       />
+                  </div>
+                  <div> <p>{item.id}</p>
+                  <p>{item.name}</p>
+                  {/* <p>{item.type}</p> */}
+                  </div>
                 </div>
-                <div>Hola</div>
               </div>
-              <div>Hola</div>
+              <div className="box-line-tree mt-5"></div>
+
             </div>
           ))}
         </div>
+     
+
 
 
         <div className="box-im-card">
@@ -54,8 +75,40 @@ export const LoadMoreCardT = () => {
               <svg xmlns="http://www.w3.org/2000/svg" width="186" height="55" viewBox="0 0 186 55"><path d="M21.4 1L1 21.4V717h264.6l20.4-20.4V1H21.4z" mask="url(#mask-2)"></path></svg>
               </span>
             </div>
+            <div><p>THE HYPE BOX</p></div>
+            <div><p>Can’t-miss news and updates from across the Marvel Universe!</p></div>
             </div>
-<div>holsa</div>
+<div className="content-load">
+{items.slice(0, visible).map((item) => (
+            <div className="box-load-card-two">
+              <div className="card-load-two">
+                <div className="int-box-card-load-two">
+                  <div className="box-load-img-two">
+                  <img
+                      className="load-img-css-two"
+                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                      alt=""
+                       />
+                  </div>
+                 
+                  {/* <div className="load-img-card">
+                      <img
+                      className="load-img-css"
+                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                      alt=""
+                       />
+                  </div> */}
+                  <div className="box-title-hype">
+                     <p>{item.id}</p>
+                  <p>{item.name}</p>
+                  {/* <p>{item.type}</p> */}
+                  </div>
+                </div>
+              </div>
+             
+            </div>
+          ))}
+</div>
 
             <div>
             <div className="rail-featured__border-bottom">
